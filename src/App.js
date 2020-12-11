227f459492901a8ffbc1cece65265758f3125b1f
Salvatore Santamaria <sam.santamaria@gmail.com>
2020-12-10 20:50:40 -0800

    Styled Table

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react'
 import Table from "./table"
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 require("es6-promise").polyfill();
 require("isomorphic-fetch");
-
 
 export default function App() {
 
@@ -13,41 +17,47 @@ export default function App() {
     fetch('https://cors-anywhere.herokuapp.com/https://fetch-hiring.s3.amazonaws.com/hiring.json')
     .then(response => response.json())
     .then(json => {
-
       const filteredData = json.filter((d) => {
         return (d.name !== null && d.name !== '')
       })
-
       filteredData.sort(function(a,b) { 
-        
-        var aListId = a.listId
-        var bListId = b.listId
-        var aName = a.name
-        var bName = b.name
-        
-          if ( aListId == bListId ) {
-            return (aName < bName) ? -1 : (aName > bName) ? 1 : 0;
-          } else {
-            return (aListId < bListId) ? -1 : 1
-          }
+        if ( a.listId == b.listId ) {
+          return (a.name < b.name) ? -1 : (a.name > b.name) ? 1 : 0;
+        } else {
+          return (a.listId < b.listId) ? -1 : 1
+        }
       })
       setData(filteredData)
     })
   }, [])
-
 
   function search(rows) {
     return rows.filter(row => row.id.toString().toLowerCase().indexOf(q) > -1)
   }
 
   return ( 
-    <div>
-      <div>
-        <input type="text" value={q} onChange={(e) => setQ(e.target.value)}/>
-      </div>
-      <div><Table data={search(data)}/></div>
-      {/* <div><Table data={data}/></div> */}
-    </div>
+    <Container fluid="md">
+      <Row>
+        <Col>
+          <h2>Fetch Rewards Coding Exercise - Front End By <a href="http://www.salvatoresantamaria.com" target="blank">Salvatore Santamaria</a></h2>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <InputGroup className="mb-3">
+            <FormControl
+              placeholder="Search By Id"
+              aria-label="Search By Id"
+              value={q} onChange={(e) => setQ(e.target.value)}
+            />
+          </InputGroup>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Table data={search(data)}/>
+        </Col>
+      </Row>
+    </Container>
   )
 }
-
--- src/table/index.js
+++ src/table/index.js
@@ -1,11 +1,11 @@
 import React from "react";
+import Table from "react-bootstrap/Table"
 
 export default function Datatable({ data }) {
   const columns = data[0] && Object.keys(data[0])
   return (
-    <table cellPadding={0} cellSpacing={0}>
+    <Table striped bordered hover>
       <thead>
-        {/* <tr>{data[0] && columns.map((heading) => <th>{heading.toString().toUpperCase()}</th>)}</tr> */}
         <tr>
           <th>Id</th>
           <th>List Id</th>
@@ -15,11 +15,9 @@ export default function Datatable({ data }) {
       <tbody>
         {data.map(row => 
         <tr>
-          {
-            columns.map(column => <td>{row[column]}</td>)
-          }
-         </tr>)}
-        </tbody>
-    </table>
+          { columns.map(column => <td>{row[column]}</td>) }
+        </tr>)}
+      </tbody>
+    </Table>
   );
 }
